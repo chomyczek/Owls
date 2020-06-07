c653dd76e14679f6180a744ab1bf9c0dab492056
Chomyczek <kaszubowski.adam@gmail.com>
2020-06-07 21:02:29 +0200

    fix camera y position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -84,7 +84,7 @@ public class PlayerController : MonoBehaviour
 		// Camera follow
 		if (mainCamera)
 		{
-			mainCamera.transform.position = new Vector3(t.position.x, cameraPos.y, cameraPos.z);
+			mainCamera.transform.position = new Vector3(t.position.x, t.position.y, cameraPos.z);
 		}
 	}
 

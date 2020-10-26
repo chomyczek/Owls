7522d80c173a5bb2902cc9f66f3ca0694f9aeccb
chomyczek <kaszubowski.adam@gmail.com>
2020-10-26 03:16:56 +0100

    add detecting gaps

--- Assets/Scripts/Enemy/EnemyActions.cs
+++ Assets/Scripts/Enemy/EnemyActions.cs
@@ -15,5 +15,30 @@ public class EnemyActions
 	{
 		enemy.Components.RigidBody.velocity =
 				new Vector2(enemy.Stats.Direction.x * enemy.Stats.Speed * Time.deltaTime, enemy.Components.RigidBody.velocity.y);
+
+		if (enemy.Stats.Direction.x != 0)
+		{
+			transform.localScale = new Vector3(enemy.Stats.Direction.x < 0 ? -1 : 1, 1, 1);
+		}
+	}
+
+	public void MoveLeftRight()
+	{
+		if (!enemy.Utilities.IsGrounded())
+		{
+			return;
+		}
+
+		if (enemy.Stats.Direction.x == 0)
+		{
+			enemy.Stats.Direction = new Vector2(1, enemy.Stats.Direction.y);
+		}
+
+		if (enemy.Utilities.DetectWall() || enemy.Utilities.DetectGap())
+		{
+			enemy.Utilities.ChangeDirection();
+		}
+
+		enemy.Actions.Move(enemy.transform);
 	}
 }
--- Assets/Scripts/Enemy/EnemyUtilities.cs
+++ Assets/Scripts/Enemy/EnemyUtilities.cs
@@ -18,32 +18,20 @@ public class EnemyUtilities
 			case EnemyEnums.Move.None:
 				break;
 			case EnemyEnums.Move.LeftRight:
-				this.MoveLeftRight();
+				enemy.Actions.MoveLeftRight();
 				break;
 			default:
 				break;
 		}
 	}
 
-	private void MoveLeftRight()
+	public bool DetectGap()
 	{
-		if(enemy.Stats.Direction.x == 0)
-		{
-			enemy.Stats.Direction = new Vector2(1, enemy.Stats.Direction.y);
-		}
-		if (enemy.Utilities.DetectWall() || enemy.Utilities.DetectGap())
-		{
-			enemy.Utilities.ChangeDirection();
-		}
-		enemy.Actions.Move(enemy.transform);
-	}
-
-	private bool DetectGap()
-	{
-		RaycastHit2D hit = Physics2D.BoxCast(
-			enemy.Components.Collider.bounds.center,
+		var ahead = enemy.Stats.Direction.x < 0 ? enemy.Components.Collider.bounds.min.x : enemy.Components.Collider.bounds.max.x;
+		RaycastHit2D hit = Physics2D.CapsuleCast(
+			new Vector2(ahead, enemy.Components.Collider.bounds.center.y),
 			enemy.Components.Collider.bounds.size,
-			//CapsuleDirection2D.Vertical,
+			CapsuleDirection2D.Vertical,
 			0,
 			Vector2.down,
 			0.1f,
@@ -52,15 +40,28 @@ public class EnemyUtilities
 		return hit.collider == null;
 	}
 
-	private bool DetectWall()
+	public bool DetectWall()
 	{
 		return false;
 		throw new NotImplementedException();
 
 	}
 
-	private void ChangeDirection()
+	public void ChangeDirection()
 	{
 		enemy.Stats.Direction = new Vector2(enemy.Stats.Direction.x > 0 ? -1 : 1, enemy.Stats.Direction.y);
+	}
+
+	public bool IsGrounded()
+	{
+		RaycastHit2D hit = Physics2D.BoxCast(
+			enemy.Components.Collider.bounds.center,
+			enemy.Components.Collider.bounds.size,
+			0,
+			Vector2.down,
+			0.1f,
+			enemy.Components.GroundLayer);
+
+		return hit.collider != null;
 	}
 }

e972753416829485a5e706f21d96bf1fde84753a
chomyczek <kaszubowski.adam@gmail.com>
2020-11-03 22:40:24 +0100

    add wall collision

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class Projectile : MonoBehaviour
 {
     [SerializeField]
@@ -15,12 +16,17 @@ public class Projectile : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private LayerMask groundLayer;
+
     private Rigidbody2D rigidbody;
-    
+    private Collider2D collider;
+
     // Start is called before the first frame update
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
         rigidbody.velocity = new Vector2(speed * rigidbody.velocity.x, rigidbody.velocity.y);
     }
 
@@ -29,11 +35,36 @@ public class Projectile : MonoBehaviour
     {
         var direction = Mathf.Sign(rigidbody.velocity.x);
         transform.Rotate(0, 0, Time.deltaTime * rotateSpeed * -direction);
+        HandleWall();
     }
 
     private void Awake()
     {
         Destroy(gameObject, selfDestroyTime);
+    }
+
+    private void HandleWall()
+	{
+		if (DetectCollider(groundLayer))
+		{
+            Destroy(gameObject);
+		}
+	}
+
+    public bool DetectCollider(LayerMask colliderLayer)
+    {
+        var capsuleSize = new Vector2(collider.bounds.size.x, collider.bounds.size.y);
+        RaycastHit2D hit = Physics2D.CapsuleCast(
+            collider.bounds.center,
+            capsuleSize,
+            CapsuleDirection2D.Vertical,
+            0,
+            new Vector2(transform.localScale.x, 0),
+            0.1f,
+            colliderLayer);
+
+        return hit.collider != null;
+
     }
 
 }
